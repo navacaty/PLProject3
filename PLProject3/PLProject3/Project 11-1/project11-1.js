c43bcaee38878102b8e914801396df021c02eacd
Tristan Miranda-Sohrabji <tristanms@utexas.edu>
2016-03-07 14:16:01 -0600

    no message

--- PLProject3/PLProject3/Project 11-1/project11-1.js
+++ PLProject3/PLProject3/Project 11-1/project11-1.js
@@ -1,28 +1,40 @@
 $(document).ready(function()  {
+	$("#customer").click(function() {
+		$(this).next().toggleClass("customer");
+		if ($(this).next().attr("class") == "customer") {
+			$("div.customer").show();
+		}
+	});
 	$("#employee").click(function() {
-
 		$(this).next().toggleClass("employee");
 		if ($(this).next().attr("class") == "employee") {
 			$("div.employee").show();
-
-
 
 		}
 
 
 	});
-		$("#customer").click(function() {
-
-			$(this).next().toggleClass("customer");
-			if ($(this).next().attr("class") == "customer") {
-				$("div.customer").show();
-
-			}
-		});
-	$("#submit").click(function() {
+	$("#submit_e").click(function() {
 		var fn = $("#e_fn").val();
-		$().text(fn);
-		alert(fn);
+		$().text("<table border = \"0\"> <tr> <td> first name: </td>
+		<td> <input type = \"text"\ id= \"e_fn\" size = \"20\" /> </td>
+			</tr>
+
+			<tr>
+			<td> last name:  </td>
+		<td> <input type = \"text"\ id = \"e_ln\" size = "20" /> </td>
+			</tr>
+
+			<tr>
+			<td> email address: </td>
+		<td> <input type = \"text"\ id = "e_email" size = "20" /> </td>
+			</tr>
+
+			<tr>
+			<td> Social security number: </td>
+		<td> <input type = \"text"\ id = "ssn" size = "20" /> </td>
+			</tr>
+			</table>");
 	});
 
 });

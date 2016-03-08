839fb126915b0c8d67579d15f098e8fa794d44b7
navacaty <navacaty@yahoo.com>
2016-03-07 23:59:45 -0600

    validiate

--- PLProject3/PLProject3/Project 11-1/project11-1.js
+++ PLProject3/PLProject3/Project 11-1/project11-1.js
@@ -14,16 +14,42 @@ $(document).ready(function()  {
 
 
 	});
-	$("#submit_e").click(function() {
+		$("#submit_e").click(function() {
 		var fn = $("#e_fn").val();
 		var ln = $("#e_ln").val();
 		var ea = $("#e_email").val();
 		var ssn = $("#ssn").val();
-		document.writeln("First name: " + fn + "<BR>");
-		document.writeln("Last name: " + ln + "<BR>");
-		document.writeln("Email address: " + ea + "<BR>");
-		document.writeln("SSN: " + ssn + "<BR>");
-		document.writeln("<a href = \"./index.html\">GO BACK NOW </a>");
+		var isValid = true;
+
+		if (fn == "") { 
+				alert("Input data into First name");
+		
+				isValid = false;
+			}
+		if (ln == "") { 
+				alert("Input data into Last name");
+		
+				isValid = false;
+			}
+		if (ea == "") { 
+				alert("Input data into Email ");
+		
+				isValid = false;
+			}
+		if (ssn == "") { 
+				alert("Input data into SSN");
+		
+				isValid = false;
+			}
+		if (isValid) {
+				document.writeln("First name: " + fn + "<BR>");
+				document.writeln("Last name: " + ln + "<BR>");
+				document.writeln("Email address: " + ea + "<BR>");
+				document.writeln("SSN: " + ssn + "<BR>");
+				document.writeln("<a href = \"./index.html\">GO BACK NOW </a>");
+						
+			} 
+	
 		});
 	$("#submit_c").click(function() {
 		var fn = $("#c_fn").val();
